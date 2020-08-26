3f223e9e9204105d8538233d5f952de88a414bef
liangwang0734 <u00u55a9vfjZYf2s1k357@cpu0547.raplab.sjc4.nvmetal.net>
2020-08-26 07:21:35 -0700

    clean up

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -7,4 +7,4 @@ fi
 host=`hostname`
 echo "sft ssh -L localhost:${port}:${host}:${port} raplab-hackathon"
 
-jupyter lab --ip=0.0.0.0 --no-browser --NotebookApp.token='' --port=8895
+jupyter lab --ip=0.0.0.0 --no-browser --NotebookApp.token='' --port=${port}
